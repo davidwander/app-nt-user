cffab111b1ca89780d4d2217bb87297360701ac7
David <dwander616@gmail.com>
2024-11-20 07:45:27 -0300

    criando pasta principal do app, config paths para pasta src, add assets

---
+++ src/app/(tabs)/index.tsx
@@ -0,0 +1,27 @@
+import { ImageBackground, Text, StyleSheet } from "react-native"
+
+export default function Home() {
+  return (
+    <ImageBackground
+      source={require("../../assets/images/bg1.jpeg")}
+      style={styles.background}
+    >
+      <Text
+        style={styles.title}
+      >
+        Welcome to the Home Screen!
+      </Text>
+    </ImageBackground>
+  )
+}
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 24,
+  },
+})
