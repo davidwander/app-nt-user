16f3bcc11313cb24cc43e92b5463886cbda897f3
David <dwander616@gmail.com>
2024-11-21 11:50:56 -0300

    criando tabs screens

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,11 +1,33 @@
-import { Tabs } from "expo-router";
+import { Tabs } from "expo-router"
+import { Octicons, AntDesign, Fontisto } from "@expo/vector-icons"
 
 export default function TabLayout() {
   return (
-    <Tabs>
-      <Tabs.Screen name="index" />
-      <Tabs.Screen name="chat" />
-      <Tabs.Screen name="search" />
+    <Tabs screenOptions={{ headerShown: false, tabBarShowLabel: false }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          tabBarIcon: ({ size, color }) =>
+            <AntDesign name="home" size={size} color={color}
+            />
+        }}
+      />
+      <Tabs.Screen
+        name="chat"
+        options={{
+          tabBarIcon: ({ size, color }) =>
+            <Fontisto name="hipchat" size={size} color={color}
+            />
+        }}
+      />
+      <Tabs.Screen
+        name="search"
+        options={{
+          tabBarIcon: ({ size, color }) => 
+            <Octicons name="search" size={size} color={color}
+            />
+        }}
+      />
     </Tabs>
   )
 }
